88280f1bd5b85c6e88fcc2a0d6a362dd7276aaf2
kelvinMulinge <kelvinmulinge9702@gmail.com>
2024-10-01 18:22:11 +0300

    Variable Scope

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = "bob";
+//function for  the global variable
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase();
+}
+var bestCustomer;
+
+// function to declare a varable as a global scope 
+function setBestCustomer () {
+ bestCustomer = 'not bob';
+return bestCustomer;
+}
+
+function overwriteBestCustomer () {
+  bestCustomer = 'maybe bob';
+  return bestCustomer;
+
+}
+
+const leastFavoriteCustomer = "value";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "intial";
+}
